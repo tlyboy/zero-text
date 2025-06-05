3ffff114accc80e01e604e651a6affe2fb952588
Guany <1084072690@qq.com>
2025-06-05 10:04:44 +0800

    优化默认布局，调整标题样式为加粗，增加文本大小，改善整体视觉效果

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,17 +1,17 @@
 <template>
   <div class="flex h-full flex-col">
     <div class="flex items-center justify-between bg-white p-4 dark:bg-black">
-      <div>🕵️ 零宽文本加密生成器</div>
+      <div class="font-bold">🕵️ 零宽文本加密生成器</div>
 
-      <div class="flex items-center gap-2">
-        <DarkToggle />
-
+      <div class="flex justify-center gap-2 text-xl">
         <a
           class="i-carbon-logo-github icon-btn"
           href="https://github.com/tlyboy/zero-text"
           target="_blank"
           rel="noopener noreferrer"
         ></a>
+
+        <DarkToggle />
       </div>
     </div>
 
