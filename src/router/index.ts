60b717ca5094bb2c4d7ac90c7f20f248e30f1031
Guany <1084072690@qq.com>
2025-11-15 18:30:10 +0800

    chore: 更新 TypeScript 至 5.9.3，确保项目依赖的兼容性和功能性，同时调整 router 配置以优化路由设置

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,7 +4,7 @@ import { setupLayouts } from 'virtual:generated-layouts'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: setupLayouts(routes),
+  routes: setupLayouts([...routes]),
 })
 
 export default router
